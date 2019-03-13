decfc4d56b6363eac01af7960e74e0342b799fdc
DESKTOP-E3ENQKH\donal <donaldshoaf@yahoo.com>
2019-03-12 22:54:38 -0400

    New IronManHUD scene with a navmesh that builds automatically as the scene data is gathered. TODO: Origin point for navmesh agent doesn't work, so pathfinding doesn't work and path can't be displayed.

---
+++ muc/Assets/FollowCamera.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class FollowCamera : MonoBehaviour
+{
+
+    public GameObject MainCamera;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    Transform cameraTransform = MainCamera.transform;
+	    Vector3 groundedCameraPosition = cameraTransform.position;
+	    groundedCameraPosition.y = 1;
+	    Quaternion groundedCameraRotation = cameraTransform.rotation;
+        transform.SetPositionAndRotation(groundedCameraPosition, groundedCameraRotation);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    Transform cameraTransform = MainCamera.transform;
+	    Vector3 groundedCameraPosition = cameraTransform.position;
+	    groundedCameraPosition.y = 1;
+        Quaternion groundedCameraRotation = cameraTransform.rotation;
+	    transform.SetPositionAndRotation(groundedCameraPosition, groundedCameraRotation);
+    }
+}
---
+++ muc/Assets/NavMeshScript.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NavMeshScript : MonoBehaviour
+{
+
+    public NavMeshSurface NavMeshSurface;
+    public GameObject CameraTracker;
+    public LineRenderer LineRenderer;
+    private NavMeshAgent _agent;
+    Vector3 destination = new Vector3(1, 1, 1);
+    public bool destinationSet = false;
+
+    void Start()
+    {
+        _agent = CameraTracker.GetComponent<NavMeshAgent>();
+    }
+    // Update is called once per frame
+	void Update () {
+		NavMeshSurface.BuildNavMesh();
+	    try
+	    {
+	        destinationSet = _agent.SetDestination(destination);
+	    }
+	    catch
+	    {
+
+	    }
+	    if (_agent.hasPath)
+	    {
+            LineRenderer.SetPositions(_agent.path.corners);
+	    }
+	}
+}
